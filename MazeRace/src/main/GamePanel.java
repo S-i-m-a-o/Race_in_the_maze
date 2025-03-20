f9d43352a093ddf0ceedf98e80807f5cf6171405
Bernard Fox <bernard.fox@student.griffith.ie>
2025-03-20 22:03:51 +0000

    Adding Thread & adding comments

--- MazeRace/src/main/GamePanel.java
+++ MazeRace/src/main/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{ //This class inherits the JPanel class
+public class GamePanel extends JPanel implements Runnable{ //This class inherits the JPanel class
 
 	//SCREEN SETTINGS
 	final int originalTileSize = 16; //16*16 tile for character / map tile etc..
@@ -13,13 +13,30 @@ public class GamePanel extends JPanel{ //This class inherits the JPanel class
 	final int tileSize = originalTileSize * scale; //48*48 pixel tiles
 	final int maxScreenColumn = 16; //16 Tiles wide
 	final int maxScreenRow = 12; //12 Tiles down
-	
 	final int screenWidth = tileSize * maxScreenColumn; //768 pixels
 	final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	
+	Thread gameThread; //Thread for concurrency
+	
+	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true); //Drawing is done in an offscreen painting buffer
+	}
+
+	public void startGameThread() {
+		gameThread = new Thread(this); //Passing GamePanel to this constructor
+		gameThread.start(); //Starts the thread.. Starts the run method
+	}
+
+	@Override
+	public void run() { //Implemented from thread for concurrency
+		
+		while(gameThread != null) { //While the game thread exists
+			
+		}
+		
 	}
 	
 }

95b59795369cd3f07ac101cd6f31f87b279195a8
Bernard Fox <bernard.fox@student.griffith.ie>
2025-03-20 22:39:19 +0000

    Creating and implementing a key handler class for keyboard

---
+++ MazeRace/src/main/KeyHandler.java
@@ -0,0 +1,77 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener{ //Listener interface for receiving keyboard events
+
+	public boolean wPressed, aPressed, sPressed, dPressed, upPressed, downPressed, leftPressed, rightPressed;
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//Not used
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		int code = e.getKeyCode(); //Returns an integer associated to a keyboard key
+		
+		if(code == KeyEvent.VK_W) {
+			wPressed = true;
+		}
+		if(code == KeyEvent.VK_A) {
+			aPressed = true;
+		}
+		if(code == KeyEvent.VK_S) {
+			sPressed = true;
+		}
+		if(code == KeyEvent.VK_D) {
+			dPressed = true;
+		}
+		if(code == KeyEvent.VK_KP_UP) {
+			upPressed = true;
+		}
+		if(code == KeyEvent.VK_KP_DOWN) {
+			downPressed = true;
+		}
+		if(code == KeyEvent.VK_KP_LEFT) {
+			leftPressed = true;
+		}
+		if(code == KeyEvent.VK_KP_RIGHT) {
+			rightPressed = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_W) {
+			wPressed = false;
+		}
+		if(code == KeyEvent.VK_A) {
+			aPressed = false;
+		}
+		if(code == KeyEvent.VK_S) {
+			sPressed = false;
+		}
+		if(code == KeyEvent.VK_D) {
+			dPressed = false;
+		}
+		if(code == KeyEvent.VK_KP_UP) {
+			upPressed = false;
+		}
+		if(code == KeyEvent.VK_KP_DOWN) {
+			downPressed = false;
+		}
+		if(code == KeyEvent.VK_KP_LEFT) {
+			leftPressed = false;
+		}
+		if(code == KeyEvent.VK_KP_RIGHT) {
+			rightPressed = false;
+		}
+	}
+
+}

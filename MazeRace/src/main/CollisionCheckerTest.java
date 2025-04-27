6f154136fde84b4879d78c64b338f884c3e50493
Bernard Fox <bernard.fox@student.griffith.ie>
2025-04-27 21:27:15 +0100

    Adding tests for detection of invisible wall

--- MazeRace/src/main/CollisionCheckerTest.java
+++ MazeRace/src/main/CollisionCheckerTest.java
@@ -8,6 +8,7 @@ import java.awt.Rectangle;
 import org.junit.jupiter.api.Test;
 
 import entity.Entity;
+import entity.Player;
 
 class CollisionCheckerTest {
 
@@ -44,5 +45,45 @@ class CollisionCheckerTest {
         checkerThree.checkTile(entityThree);
         assertTrue(entityThree.collisionOn);
         
+        //===============================================================
+        //Tests for Milestone Three.. Invisible walls.
+        
+        
+        //This test passes because the tile directly south of it has its collision on.
+        GamePanel leftSide = new GamePanel();
+        Entity leftSideEntity = new Entity();
+        leftSideEntity.worldX = 6*gp.tileSize;
+        leftSideEntity.worldY = 11*gp.tileSize;
+        leftSideEntity.solidArea = new Rectangle(0,0,32,32);
+        leftSideEntity.direction = "down";
+        CollisionChecker leftSideChecker = new CollisionChecker(leftSide);;
+        leftSideChecker.checkTile(leftSideEntity);
+        assertTrue(leftSideEntity.collisionOn);
+        
+        
+        //This test passes because the tile directly south has its collision off. (invisible wall)
+        GamePanel middle = new GamePanel();
+        Entity middleEntity = new Entity();
+        middleEntity.worldX = 6*gp.tileSize;
+        middleEntity.worldY = 12*gp.tileSize;
+        middleEntity.solidArea = new Rectangle(0,0,32,32);
+        middleEntity.direction = "down";
+        CollisionChecker middleChecker = new CollisionChecker(middle);
+        middleChecker.checkTile(middleEntity);
+        assertFalse(middleEntity.collisionOn);
+        
+        
+        //This test passes because the tile directly south has its collision on.
+        GamePanel rightSide = new GamePanel();
+        Entity rightSideEntity = new Entity();
+        rightSideEntity.worldX = 6*gp.tileSize;
+        rightSideEntity.worldY = 13*gp.tileSize;
+        rightSideEntity.solidArea = new Rectangle(0,0,32,32);
+        rightSideEntity.direction = "down";
+        CollisionChecker rightSideChecker = new CollisionChecker(rightSide);
+        rightSideChecker.checkTile(rightSideEntity);
+        assertTrue(rightSideEntity.collisionOn);
+
+       
     }
 }
